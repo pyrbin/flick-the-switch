dd7faad20b2e645f9b12c5d1eb9baba2cd721e2d
smultron <fredholmisak@outlook.com>
2024-06-09 16:04:41 +0200

    add audio package, audio assets

---
+++ roguelight/Assets/Data/Audio/AudioEnums - AudioLibrary.cs
@@ -0,0 +1,15 @@
+namespace Audio {
+    public enum Sounds {
+        SwitchClick,
+        EnemyBlueDeath,
+        ClickFail,
+        ClickSuccess,
+        ShopHover,
+        ShopReroll,
+        ShopUpgrade
+    }
+}
+namespace Audio {
+    public enum Music {
+    }
+}
